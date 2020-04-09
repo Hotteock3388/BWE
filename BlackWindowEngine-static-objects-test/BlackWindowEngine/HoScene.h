ae2cd1e917b5e7e21c9316d4cf85a38f65d843a4
Song <217.Songs.SH@gmail.com>
2020-04-09 20:07:32 +0900

    b

---
+++ BlackWindowEngine-static-objects-test/BlackWindowEngine/HoScene.h
@@ -0,0 +1,65 @@
+#pragma once
+#pragma once
+
+#include "Canon.h"
+#include "SceneManager.h"
+#include <optional>
+#include "Player.h"
+#include "Wall.h"
+#include "PushTrap.h"
+#include "MovingTrap.h"
+
+using namespace std;
+
+class HoScene : public SceneManager
+{
+private:
+public:
+	
+	int Size = 30;
+	HoScene() : SceneManager()
+	{
+		mapPointer = new Map(30, 30);
+	};
+	void Create()
+	{
+		Map_Make();
+		
+
+	}
+
+	void Render(float dt)
+	{
+		//		worldOutliner.FindObject("MovingTrap_1")->TryWork();
+
+			//dynamic_cast<Player*>(worldOutliner.FindObject("canon1"))->getW();
+
+		
+
+
+	}
+	void Resize(int x, int y)
+	{
+
+	}
+	void Input(float dt)
+	{
+
+	}
+	void Pause()
+	{
+
+	}
+	~HoScene()
+	{
+
+	}
+	void Map_Make()
+	{
+		//worldOutliner.AddObject(new Wall(FPosition({ 0,0 }), "Wall_Top", "бс", Area({ Size, 1 }), "Wall"));
+		//worldOutliner.AddObject(new Wall(FPosition({ 0,0 }), "Wall_Left", "бс", Area({ 1, Size }), "Wall"));
+		//worldOutliner.AddObject(new Wall(FPosition({ (float)Size - 1, 0 }), "Wall_Right", "бс", Area({ 1, Size }), "Wall"));
+		//worldOutliner.AddObject(new Wall(FPosition({ 0, (float)Size - 1 }), "Wall_Bottom", "бс", Area({ Size, 1 }), "Wall"));
+	}
+
+};
